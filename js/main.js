7cb8ac501640f219b47b0875fff6a4079f49c7ff
jeelrajani <jeelrajani1111@gmail.com>
2026-01-09 16:58:50 +0530

    c137

--- js/main.js
+++ js/main.js
@@ -231,20 +231,3 @@ document.querySelectorAll("a, button, .clickable").forEach(el => {
     });
 });
 
-
-/* =======================
-   PROFILE IMAGE PARALLAX
-======================= */
-// document.querySelectorAll(".profile-wrap").forEach(el => {
-//     el.addEventListener("mousemove", (e) => {
-//         const rect = el.getBoundingClientRect();
-//         const x = e.clientX - rect.left - rect.width / 2;
-//         const y = e.clientY - rect.top - rect.height / 2;
-
-//         el.style.transform = `translate(${x * 0.05}px, ${y * 0.05}px)`;
-//     });
-
-//     el.addEventListener("mouseleave", () => {
-//         el.style.transform = "translate(0,0)";
-//     });
-// });
